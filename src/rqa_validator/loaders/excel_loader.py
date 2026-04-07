e4d93092f4a0179237c25e4826ec981decd9ba67
Greg <gregory.sabella@impact-initiatives.org>
2026-04-07 14:33:53 +0300

    simplify excel sheet checking

--- src/rqa_validator/loaders/excel_loader.py
+++ src/rqa_validator/loaders/excel_loader.py
@@ -29,52 +29,62 @@ class ExcelLoaderData:
     
 
 class ExcelLoader:
+
     def __init__(self, schema_config: BaseDatasetSchema):
         self.schema = schema_config
 
     def load(self, filepath: Path)  -> ExcelLoaderData:
-        """Load matching sheets from Excel file."""
+
         all_sheets = fastexcel.read_excel(filepath).sheet_names
         all_sheets = list(map(str.lower, all_sheets))
         
         data = ExcelLoaderData()
         
         for sheet_name in all_sheets:
-            if self._should_load_sheet(sheet_name):
+            if (mapped_name := self._should_load_sheet(sheet_name)):
                 df: pl.DataFrame = pl.read_excel(source=filepath, sheet_name=sheet_name)
                 # TODO: check for duplicate column names. difficult as they are renames on load
                 df = df.rename(str.lower)
                 
-                mapped_name = self._get_mapped_name(sheet_name, self.schema.loaded_sheets)
+                # mapped_name = self._get_mapped_name(sheet_name, self.schema.loaded_sheets)
                 
                 data.loaded_sheets.append(LoadedSheet(mapped_name=mapped_name,
                                                       original_name=sheet_name,
                                                       data=df))                
 
-            elif self._should_ignore_sheet(sheet_name):
+            elif (mapped_name := self._should_ignore_sheet(sheet_name)):
                 # unexpected sheet
-                mapped_name = self._get_mapped_name(sheet_name, self.schema.unloaded_sheets)
+                # mapped_name = self._get_mapped_name(sheet_name, self.schema.unloaded_sheets)
                 data.unloaded_sheets.append(mapped_name)
             else:
                 data.unexpected_sheets.append(sheet_name)
 
         return data
     
-    def _get_mapped_name(self, sheet_name: str, sheets: List[SheetMapping]) -> str:
-        """Get standardized name for sheet."""
-        for sheet_config in sheets:
-            if sheet_name in sheet_config.names:
-                return sheet_config.standard_name
-        return sheet_name
-    
     def _should_load_sheet(self, sheet_name: str) -> bool:
-        """Check if sheet name matches any configured names."""
+        """Check if sheet name matches any configured/expected names
+            that are to be loaded.
+
+        Args:
+            sheet_name (str): Sheet name to be checked
+
+        Returns:
+            bool: _description_
+        """
         for sheet_config in self.schema.loaded_sheets:
             if sheet_config.matches(sheet_name):
                 return sheet_config.standard_name
     
     def _should_ignore_sheet(self, sheet_name: str) -> bool:
-        """Check if sheet name matches any configured names."""
+        """Check if sheet name matches any configured names
+            that should be ignored/not loaded.
+
+        Args:
+            sheet_name (str): Sheet name to be checked
+
+        Returns:
+            bool: _description_
+        """
         for sheet_config in self.schema.unloaded_sheets:
             if sheet_config.matches(sheet_name):
                 return sheet_config.standard_name
