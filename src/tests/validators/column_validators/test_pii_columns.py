68e255f2037bbd5e9192702c83961256a0514d2c
Greg <gregory.sabella@impact-initiatives.org>
2026-04-23 09:32:35 +0300

    update pii tests

--- src/tests/validators/column_validators/test_pii_columns.py
+++ src/tests/validators/column_validators/test_pii_columns.py
@@ -1,6 +1,7 @@
 import pytest
 import polars as pl
 
+from rqa_validator.loaders.base import ColumnMap
 from rqa_validator.loaders.excel_loader import SheetMap, ExcelLoaderData
 from rqa_validator.validators.column_validators import PiiColumns
 from rqa_validator.validators.base import BaseValidator
@@ -59,6 +60,44 @@ def invalid_fuzzy_excel_data():
     
     return ExcelLoaderData(loaded_sheets=[loaded_sheet])
 
+@pytest.fixture
+def invalid_excel_data2():
+    """Create ExcelLoaderData with matching columns"""
+    df = pl.DataFrame({
+        "uuid": [1, 2, 3, 4, 5],
+        "some_column": ['a@b.com', '2', '3', '4', '5'],
+        "another_column": ['1', '2', '3', '4', '0557456783'],
+    })
+    
+    loaded_sheet = SheetMap(
+        schema_sheet_name="raw_data",
+        data_sheet_name="raw_data",
+        data=df,
+        data_columns=["uuid"]
+    )
+    
+    return ExcelLoaderData(loaded_sheets=[loaded_sheet])
+
+@pytest.fixture
+def invalid_excel_data3():
+    """Create ExcelLoaderData with matching columns"""
+    df = pl.DataFrame({
+        "uuid": [1, 2, 3, 4, 5],
+        "some_column": ['a@b.com', '2', '3', '4', '5'],
+        "another_column": ['1', '2', '3', '4', '0557456783'],
+    })
+    
+    loaded_sheet = SheetMap(
+        schema_sheet_name="raw_data",
+        data_sheet_name="raw_data",
+        data=df,
+        data_columns=["uuid"],
+        column_map=[ColumnMap(data_column_name='uuid',
+                              schema_column_name='uuid')]
+    )
+    
+    return ExcelLoaderData(loaded_sheets=[loaded_sheet])
+
 class TestPiiColumns():
     def test_valid_data(self, validator: BaseValidator,
                         valid_excel_data: ExcelLoaderData):
@@ -80,3 +119,19 @@ class TestPiiColumns():
         
         assert isinstance(result, list)
         assert len(result) == 1
+
+    def test_invalid_data2(self, validator: BaseValidator,
+                        invalid_excel_data2: ExcelLoaderData):
+        result = validator.validate(invalid_excel_data2)
+        
+        assert isinstance(result, list)
+        assert len(result) == 1
+        assert len(result[0].details['row_index']) == 2
+
+    def test_invalid_data3(self, validator: BaseValidator,
+                        invalid_excel_data3: ExcelLoaderData):
+        result = validator.validate(invalid_excel_data3)
+        
+        assert isinstance(result, list)
+        assert len(result) == 1
+        assert len(result[0].details['uuid']) == 2
